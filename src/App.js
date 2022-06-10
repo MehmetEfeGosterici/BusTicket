9e7faa931027de3120babbaa658c24c1cc7bc05f
Mehmet Efe <efe.gosterici@bulutmd.com>
2022-06-10 21:48:53 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import { ImLocation } from "react-icons/im"
+import { FaChevronDown } from "react-icons/fa"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <>
+    <div className='navbar'>
+      <div className='navbar-container'>
+        <div className='navbar-title'>
+          <p><b>öbilet</b>.com</p>
+        </div>
+        <div className='navbar-items'>
+          <p>Üye Girişi</p>
+          <p>Biletlerim</p>
+          <p>Yardım</p>
+        </div>
+      </div>
     </div>
-  );
+    <div className='body'>
+      <div className='Input-container'>
+        <div className='Input'>
+          <p>Kalkış Noktası</p>
+          <div>
+            <ImLocation color='grey' size={25} />
+            <input value={"Edirne"} />
+            <FaChevronDown color='grey' size={25} />
+          </div>
+        </div>
+        <div className='Input-border' ></div>
+        <div className='Input'>
+          <p>Varış Noktası</p>
+          <div>
+            <ImLocation color='grey' size={25} />
+            <input value={"Ankara"} />
+            <FaChevronDown color='grey' size={25} />
+          </div>
+        </div>
+        <div className='Input-border' ></div>
+        <div className='Input'>
+          <p>Yolculuk Tarihi</p>
+          <div>
+            <div>
+              <ImLocation color='grey' size={25} />
+              <input value={"Tarih"} />
+            </div>
+            <div className='today-tomorrow'>
+              <p>Bugün</p>
+              <p>Yarın</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </>
 }
 
 export default App;
